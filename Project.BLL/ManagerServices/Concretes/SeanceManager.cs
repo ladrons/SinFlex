1af909adecab034742f24d9b9edeb79c3886b433
Burak Çevik <burak_cevik76@hotmail.com.tr>
2023-02-11 17:24:49 +0300

    Added new Rep/Man services

---
+++ Project.BLL/ManagerServices/Concretes/SeanceManager.cs
@@ -0,0 +1,21 @@
+﻿using Project.BLL.ManagerServices.Abstracts;
+using Project.DAL.Repositories.Abstracts;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.ManagerServices.Concretes
+{
+    public class SeanceManager : BaseManager<Seance>, ISeanceManager
+    {
+        ISeanceRepository _seanceRep;
+
+        public SeanceManager(ISeanceRepository seanceRep) : base(seanceRep)
+        {
+            _seanceRep = seanceRep;
+        }
+    }
+}
